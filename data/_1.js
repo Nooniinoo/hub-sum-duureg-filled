eba672879a65b4f22b1bb0f96043bf89e1746233
Nomin-erdene <nomin-erdene@Nomin-erdenes-MacBook-Air.local>
2025-05-26 05:15:27 +0800

    LFS

---
+++ data/_1.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dcee39c6b79feab3a58062c7005109892f856b9cb290e8aea961b197ae57c0fe
+size 35301183
---
+++ data/_2.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9e1817362701eaa1dc591abcf08c20877d6b5c48f343b8bd15a2de011a9c5797
+size 2549006
